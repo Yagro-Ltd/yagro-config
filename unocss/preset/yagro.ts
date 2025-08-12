88b54ba212c2a8e945bdfa279cc059b1111deed3
Marc <m.horne@MacBookPro.Home>
2025-08-12 13:51:13 +0100

    use yagro preset like before

--- unocss/preset/yagro.ts
+++ unocss/preset/yagro.ts
@@ -1,40 +1,14 @@
-import { definePreset, presetIcons, presetUno, presetWebFonts } from 'unocss';
-import { shortcuts } from './shortcuts';
-import { getPreflights } from './preflights';
-import { getTheme } from './theme';
+import { definePreset } from 'unocss'
+import { shortcuts } from './shortcuts.ts'
+import { getPreflights } from './preflights.ts'
+import { getTheme } from './theme.ts'
 
-export interface YagroPresetOptions {
-  prefix?: string;
-  fonts?: { provider?: 'google' | 'none' };
-  icons?: Record<string, string>;
-}
-
-export default definePreset<YagroPresetOptions>((options = {}) => {
-  const { prefix = 'ÿ-', fonts = { provider: 'google' }, icons = {} } = options;
-
-  const theme = getTheme();
-
+export default definePreset(() => {
+  const theme = getTheme()
   return {
     name: 'yagro',
-    prefix,
-    presets: [
-      presetUno(),
-      presetIcons({ collections: { custom: icons } }),
-      ...(fonts.provider === 'google'
-        ? [
-            presetWebFonts({
-              provider: 'google',
-              fonts: {
-                montserrat: [
-                  { name: 'Montserrat', weights: ['400', '600', '700'], italic: true },
-                ],
-              },
-            }),
-          ]
-        : []),
-    ],
     shortcuts,
     preflights: getPreflights({ theme }),
     theme,
-  };
-});
+  }
+})
