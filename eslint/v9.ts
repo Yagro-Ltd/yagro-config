f876e548f83c082f5c35d67c2d0f1beb2b0b9811
Marc <m.horne@MacBookPro.Home>
2025-08-18 14:26:56 +0100

    eslint update jsonc

--- eslint/v9.ts
+++ eslint/v9.ts
@@ -1,16 +1,17 @@
-import eslintPluginJsonc from 'eslint-plugin-jsonc';
+// eslint.config.js
+import jsonc from 'eslint-plugin-jsonc';
 import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
 import pluginSortKeysFix from 'eslint-plugin-sort-keys-fix';
 import pluginStorybook from 'eslint-plugin-storybook';
 import pluginVue from 'eslint-plugin-vue';
 import vueParser from 'vue-eslint-parser';
 
-import json from '@eslint/json';
 import * as tsParser from '@typescript-eslint/parser';
 
 export default [
   {
-    ...pluginVue.configs['flat/recommended+typescript+setup' as keyof typeof pluginVue.configs],
+    // Vue + TS
+    ...pluginVue.configs['flat/recommended+typescript+setup'],
     files: ['**/*.{ts,vue}'],
     ignores: ['node_modules/**', '**/package.json'],
     languageOptions: {
@@ -23,81 +24,73 @@ export default [
         useNuxtApp: 'readonly',
       },
       parser: vueParser,
-      parserOptions: {
-        parser: tsParser,
-        sourceType: 'module',
-      },
+      parserOptions: { parser: tsParser, sourceType: 'module' },
     },
     plugins: {
-      json,
       'simple-import-sort': pluginSimpleImportSort,
       'sort-keys-fix': pluginSortKeysFix,
       storybook: pluginStorybook,
       vue: pluginVue,
     },
     rules: {
-      "eol-last": ["error", "always"],
-      "max-len": ["error", {
+      'eol-last': ['error', 'always'],
+      'max-len': ['error', {
         code: 100,
         ignoreRegExpLiterals: true,
         ignoreStrings: true,
         ignoreTemplateLiterals: true,
         ignoreTrailingComments: true,
         ignoreUrls: true,
-        tabWidth: 2
+        tabWidth: 2,
       }],
       'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': [
-        'error',
-        {
-          groups: [
-            ['^node:', '^\\w'],
-            [''],
-            ['^@/components'],
-            [''],
-            ['^@/composables'],
-            [''],
-            ['^@/stores'],
-            [''],
-            ['^@/'],
-            [''],
-            ['^~/'],
-            ['^~~/'],
-            [''],
-            ['^#ui/'],
-            [''],
-            ['^#/'],
-            [''],
-            ['^\\u0000', '^\\.', '^\\.\\./'],
-            [''],
-            ['^.+\\.d\\.ts$'],
-          ],
-        },
-      ],
+      'simple-import-sort/imports': ['error', {
+        groups: [
+          ['^node:', '^\\w'],
+          [''],
+          ['^@/components'],
+          [''],
+          ['^@/composables'],
+          [''],
+          ['^@/stores'],
+          [''],
+          ['^@/'],
+          [''],
+          ['^~/'],
+          ['^~~/'],
+          [''],
+          ['^#ui/'],
+          [''],
+          ['^#/'],
+          [''],
+          ['^\\u0000', '^\\.', '^\\.\\./'],
+          [''],
+          ['^.+\\.d\\.ts$'],
+        ],
+      }],
       'sort-keys-fix/sort-keys-fix': 'warn',
-      'vue/attributes-order': [
-        'error',
-        {
-          alphabetical: false,
-          order: [
-            'CONDITIONALS',
-            'GLOBAL',
-            'UNIQUE',
-            'DEFINITION',
-            'LIST_RENDERING',
-            'RENDER_MODIFIERS',
-            'TWO_WAY_BINDING',
-            'OTHER_DIRECTIVES',
-            'CONTENT',
-            'ATTR_STATIC',
-            'ATTR_DYNAMIC',
-            'ATTR_SHORTHAND_BOOL',
-            'EVENTS',
-          ],
-        },
-      ],
+      'vue/attributes-order': ['error', {
+        alphabetical: false,
+        order: [
+          'CONDITIONALS',
+          'GLOBAL',
+          'UNIQUE',
+          'DEFINITION',
+          'LIST_RENDERING',
+          'RENDER_MODIFIERS',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'CONTENT',
+          'ATTR_STATIC',
+          'ATTR_DYNAMIC',
+          'ATTR_SHORTHAND_BOOL',
+          'EVENTS',
+        ],
+      }],
     },
   },
+
+  // Vue file indentation specifics
   {
     files: ['**/*.vue'],
     rules: {
@@ -106,40 +99,41 @@ export default [
       'vue/script-indent': ['error', 2, { baseIndent: 1, ignores: [], switchCase: 1 }],
     },
   },
+
+  // TS indentation
   {
     files: ['**/*.ts'],
     rules: {
       indent: ['error', 2],
     },
   },
-  ...eslintPluginJsonc.configs['flat/recommended-with-jsonc'],
+
+  // JSON (strict JSON)
   {
     files: ['**/*.json'],
-    plugins: {
-      json,
-      'sort-keys-fix': pluginSortKeysFix,
-    },
+    ignores: ['**/package.json'],
+    languageOptions: { parser: jsonc.parsers.json },
+    plugins: { jsonc, 'sort-keys-fix': pluginSortKeysFix },
     rules: {
+      'eol-last': ['error', 'always'],
       'jsonc/indent': ['error', 2],
       'jsonc/sort-keys': [
         'error',
-        {
-          order: ['name', 'version', 'description', 'license'],
-          pathPattern: '^$',
-        },
-        {
-          order: { type: 'asc' },
-          pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
-        },
+        { order: ['name', 'version', 'description', 'license'], pathPattern: '^$' },
+        { order: { type: 'asc' }, pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$' },
       ],
-      'sort-keys-fix/sort-keys-fix': [
-        'warn',
-        'asc',
-        {
-          caseSensitive: false,
-          natural: true,
-        },
-      ],
+      'sort-keys-fix/sort-keys-fix': ['warn', 'asc', { caseSensitive: false, natural: true }],
+    },
+  },
+
+  // JSON with Comments (jsonc)
+  {
+    files: ['**/*.jsonc'],
+    languageOptions: { parser: jsonc.parsers.jsonc },
+    plugins: { jsonc },
+    rules: {
+      'eol-last': ['error', 'always'],
+      'jsonc/indent': ['error', 2],
     },
   },
 ];
